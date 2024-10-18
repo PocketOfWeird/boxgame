abab01af6b481b9f1aa92e86b675a8d5f5f5d441
pocketofweird <hartzlernathan@gmail.com>
2024-10-18 16:10:32 -0500

    model and styles complete

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, input, text)
-import Html.Attributes exposing (autofocus, value)
+import Html exposing (Html, audio, div, img, input, source, text)
+import Html.Attributes exposing (autofocus, autoplay, class, height, src, style, value)
 import Html.Events exposing (onInput)
 import Random
 import Random.List
@@ -24,35 +24,42 @@ main =
 type Model 
   = WaitingForInput
   | ShowingTrick Trick
-  | ShowingSuccessClip Clip
+  | ShowingSuccessScreen Screen
 
 
 type alias Trick =
+  { boxtroll : String
+  , background : String
+  , text : String
+  , successScreen : String
+  }
+
+type alias PickedTrick =
   { boxtroll : (Maybe String, List String)
   , background : (Maybe String, List String)
   , text : (Maybe String, List String)
-  , successClip : (Maybe String, List String)
+  , successScreen : (Maybe String, List String)
   }
 
-type alias Clip =
+type alias Screen =
   String
 
 boxtrolls : List String
 boxtrolls = 
-  [ "img/boxtroll_fish.jpg"
-  , "img/boxtroll_fragile.jpg"
-  , "img/boxtroll_oilcan.jpg"
-  , "img/boxtroll_shoe.jpg"
-  , "img/boxtroll_sparky.jpg"
-  , "img/boxtroll_wheels.jpg"
+  [ "boxtroll_fish"
+  , "boxtroll_fragile"
+  , "boxtroll_oilcan"
+  , "boxtroll_shoe"
+  , "boxtroll_sparky"
+  , "boxtroll_wheels"
   ]
 
 backgrounds : List String
 backgrounds =
-  [ "img/bkg1.jpg"
-  , "img/bkg2.jpg"
-  , "img/bkg3.jpg"
-  , "img/bkg4.jpg"
+  [ "bkg1"
+  , "bkg2"
+  , "bkg3"
+  , "bkg4"
   ]
 
 trickTexts : List String
@@ -72,18 +79,19 @@ trickTexts =
   , "Blah Blah 13"
   , "Blah Blah 14"
   , "Blah Blah 15"
-  , "Easy Trick 16"
   ]
 
-successClips : List String
-successClips =
-  [ "01.mp4"
-  , "02.mp4"
-  , "03.mp4"
-  , "04.mp4"
-  , "05.mp4"
-  , "06.mp4"
+successScreens : List String
+successScreens =
+  [ "SuccessScreen01"
+  , "SuccessScreen02"
+  , "SuccessScreen03"
+  , "SuccessScreen04"
   ]
+
+easyTrick : Trick
+easyTrick =
+  Trick "boxtroll_fish" "bkg1" "Easy Trick 16 with lots of words. Oh my, so many words!" "SuccessScreen01"
 
 init : () -> (Model, Cmd Msg)
 init _ =
@@ -98,7 +106,7 @@ init _ =
 
 type Msg 
   = KeyboardInput String
-  | TrickPicked Trick
+  | TrickPicked PickedTrick
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -108,56 +116,80 @@ update msg model =
     KeyboardInput number -> 
       case number of
         "4" ->
-          ( ShowingTrick (Trick (Just "img/boxtroll_wheels.jpg", []) (Just "img/bkg1.jpg", []) (Just "Easy Trick 16", []) (Just "06.mp4", []))
-          , Cmd.none
-          )
+          case model of
+            WaitingForInput ->
+              ( ShowingTrick easyTrick
+              , Cmd.none
+              )
+
+            _ ->
+              doNothingWith model
         
         "s" ->
           case model of
-            ShowingTrick trick -> 
-              case (Tuple.first trick.successClip) of
-                Just successClip ->
-                  ( ShowingSuccessClip successClip
-                  , Cmd.none
-                  )
-                Nothing -> (model, Cmd.none)
+            ShowingTrick trick ->   
+              ( ShowingSuccessScreen trick.successScreen
+              , Cmd.none
+              )
 
-            _ -> (model, Cmd.none)
+            _ -> 
+              doNothingWith model
+
+        "w" ->
+          ( WaitingForInput
+          , Cmd.none
+          )
 
         _ ->
-          ( model
-          , Random.generate TrickPicked pickRandomTrick
-          )
+          case model of
+            WaitingForInput ->
+              ( model
+              , Random.generate TrickPicked pickRandomTrick
+              )
+
+            _ ->
+              doNothingWith model
     
-    TrickPicked trick ->
-      ( ShowingTrick trick
+    TrickPicked pickedTrick ->
+      ( ShowingTrick (toTrick pickedTrick)
       , Cmd.none
       )
 
 
+doNothingWith : Model -> (Model, Cmd Msg)
+doNothingWith model = 
+  ( model
+  , Cmd.none
+  )
 
-
-pickRandomTrick : Random.Generator Trick
+pickRandomTrick : Random.Generator PickedTrick
 pickRandomTrick =
-  Random.map4 Trick sixBoxtrolls fourBackgrounds sixteenTricks sixSuccessClips
+  Random.map4 PickedTrick pickBoxtrolls pickBackgrounds pickTrickText pickSuccessScreens
 
-sixBoxtrolls : Random.Generator (Maybe String, List String)
-sixBoxtrolls =
+pickBoxtrolls : Random.Generator (Maybe String, List String)
+pickBoxtrolls =
   Random.List.choose boxtrolls
 
-fourBackgrounds : Random.Generator (Maybe String, List String)
-fourBackgrounds =
+pickBackgrounds : Random.Generator (Maybe String, List String)
+pickBackgrounds =
   Random.List.choose backgrounds
 
-sixteenTricks : Random.Generator (Maybe String, List String)
-sixteenTricks =
+pickTrickText : Random.Generator (Maybe String, List String)
+pickTrickText =
   Random.List.choose trickTexts
 
-sixSuccessClips : Random.Generator (Maybe String, List String)
-sixSuccessClips =
-  Random.List.choose successClips
+pickSuccessScreens : Random.Generator (Maybe String, List String)
+pickSuccessScreens =
+  Random.List.choose successScreens
 
 
+toTrick : PickedTrick -> Trick
+toTrick pickedTrick =
+  { boxtroll = Maybe.withDefault "" (Tuple.first pickedTrick.boxtroll)
+  , background = Maybe.withDefault "" (Tuple.first pickedTrick.background)
+  , text = Maybe.withDefault "" (Tuple.first pickedTrick.text)
+  , successScreen = Maybe.withDefault "" (Tuple.first pickedTrick.successScreen)
+  }
 
 
 -- SUBSCRIPTIONS
@@ -174,23 +206,75 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  div [] 
-      [ div [] [ text "The BoxGame"]
-      , div [] [ input [autofocus True, value "", onInput KeyboardInput] []]
+  div [ style "background-color" "black" ] 
+      [ div [ style "display" "flex", style "width" "100%", style "justify-content" "center"] 
+            [ input [autofocus True, value "", onInput KeyboardInput, style "background-color" "black" ] []]
       , case model of
           WaitingForInput ->
-            div [] [ text "Waiting for input" ]
+            div [] [ img ([ src (toImgRef "cover") ] ++ fullScreenImageStyle) []]
           
           ShowingTrick trick ->
             div [] 
-                [ div [] [ text (Maybe.withDefault "boxtroll" (Tuple.first trick.boxtroll)) ] 
-                , div [] [ text (Maybe.withDefault "background" (Tuple.first trick.background)) ] 
-                , div [] [ text (Maybe.withDefault "text" (Tuple.first trick.text)) ] 
-                , div [] [ text (Maybe.withDefault "successClip" (Tuple.first trick.successClip)) ] 
+                [ div [] 
+                      [ img 
+                          [ src (toImgRef trick.boxtroll)
+                          , style "position" "absolute"
+                          , style "bottom" "0px"
+                          , style "left" "200px"
+                          , style "width" "250px"
+                          , style "z-index" "20"
+                          ] 
+                          []
+                      , viewAudio trick.boxtroll
+                      ] 
+                , div [] 
+                      [ img 
+                          ([ src (toImgRef trick.background)
+                          , style "z-index" "10" ] 
+                          ++ fullScreenImageStyle
+                          ) 
+                          [] 
+                      ] 
+                , div [] 
+                      [ Html.h2 
+                          [ style "position" "absolute"
+                          , style "top" "200px"
+                          , style "right" "50px"
+                          , style "width" "1350px"
+                          , style "z-index" "30" 
+                          , style "font-size" "100px"
+                          , style "color" "LemonChiffon"
+                          , style "text-shadow" "10px 10px 10px black"
+                          , style "font-family" "Copperplate, Papyrus, fantasy"
+                          , style "font-weight" "bold"
+                          , style "word-wrap" "break-word"
+                          , style "white-space" "normal"
+                          ] 
+                          [ text trick.text 
+                          ] 
+                      ]
                 ]
-          ShowingSuccessClip clip ->
-            div [] [ text ("Success Clip: " ++ clip) ]
+          ShowingSuccessScreen screen ->
+            div [] 
+                [ img ([ src (toImgRef screen) ] ++ fullScreenImageStyle) [] 
+                , viewAudio screen
+                ]
 
       ]
 
+viewAudio : String -> Html msg
+viewAudio file = 
+  audio [ autoplay True ]
+        [ source [ src (toAudioRef file), Html.Attributes.type_ "audio/mpeg" ] [] ] 
 
+fullScreenImageStyle : List (Html.Attribute msg)
+fullScreenImageStyle =
+  [style "position" "absolute", style "top" "25px", style "width" "100%"]
+
+toImgRef : String -> String
+toImgRef file =
+  "img/" ++ file ++ ".jpg"
+
+toAudioRef : String -> String
+toAudioRef file =
+  "aud/" ++ file ++ ".mp3"
